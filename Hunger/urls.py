28be7c8e69410b2188aa7397f78d8bd9cf8f0bee
Sahil <sahilkalra1993@gmail.com>
2017-06-07 16:30:18 +0530

    Added media

--- Hunger/urls.py
+++ Hunger/urls.py
@@ -9,3 +9,7 @@ urlpatterns = [
     url(r'^menu/',include('menu.urls')),
     url(r'^home/',include('home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
